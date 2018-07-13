7907af98b4f25d96353943346fd3f90457dbacb3
Atsushi Tadokoro <tadokoro@gmail.com>
2018-07-13 23:23:28 +0900

    update

--- init.el
+++ init.el
@@ -31,11 +31,9 @@
 ;;(setq default-input-method "japanese-skk")
 ;;(require 'skk-study))
 
-
 ;;font
 (set-face-attribute 'default nil :family "SF Mono" :height 120)
 (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP"))
-;;(setq face-font-rescale-alist '(("Meiryo" . 0.85)))
 
 ;;cursor
 (setq-default cursor-type '(bar . 3))
@@ -68,15 +66,3 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (markdown-mode haskell-mode auto-complete))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
